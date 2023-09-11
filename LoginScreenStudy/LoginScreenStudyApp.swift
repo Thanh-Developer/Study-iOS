eda4b4d6b75ff8d1504e8e064535d1980cf76b9a
JohnPham <ThanhP@joblogic.com>
2023-09-11 22:51:48 +0700

    Initial Commit

---
+++ LoginScreenStudy/LoginScreenStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenStudyApp.swift
+//  LoginScreenStudy
+//
+//  Created by JohnPham on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
